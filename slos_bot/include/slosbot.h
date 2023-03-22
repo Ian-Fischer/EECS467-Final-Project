d685454cdd3f07bde0c87d64b6aa2a300bad626a
Ashwin Gupta <ashwingupta2000@gmail.com>
2023-03-22 17:03:55 -0400

    pub and sub

--- slos_bot/include/slosbot.h
+++ slos_bot/include/slosbot.h
@@ -1,10 +1,12 @@
 #include "ros/node_handle.h"
 #include "ros/ros.h"
+#include "sensor_msgs/Image.h"
+#include <image_transport/image_transport.h>
 
 class SLOSBot 
 {
     public:
-        SLOSBot() = default;
+        SLOSBot();
     private:
         enum class State {
             SEARCH_FOR_OBJECT,
@@ -13,8 +15,15 @@ class SLOSBot
         State state = State::SEARCH_FOR_OBJECT;
 
         ros::NodeHandle nh;
+        ros::Publisher motor_command_pub; 
+
+        ros::Subscriber depth_img_sub;
+        ros::Subscriber rgb_img_sub;
 
         void search_for_object();
 
         void execute_sm();
+
+        void depth_img_cb(sensor_msgs::ImageConstPtr img);
+        void rgb_img_cb(sensor_msgs::ImageConstPtr img);
 };
--- slos_bot/src/main.cpp
+++ slos_bot/src/main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include "ros/init.h"
+#include "ros/publisher.h"
 #include "slosbot.h"
+#include "lcm_to_ros/mbot_motor_command_t.h"
+
+SLOSBot::SLOSBot() :
+ motor_command_pub( nh.advertise<lcm_to_ros::mbot_motor_command_t>("lcm_to_ros/MBOT_MOTOR_COMMAND_republisher", 1)),
+ depth_img_sub( nh.subscribe("camera/depth_registered/image_raw", 1, &SLOSBot::depth_img_cb, this) ),
+ rgb_img_sub( nh.subscribe("camera/rgb/image_rect_color", 1, &SLOSBot::rgb_img_cb, this) ) 
+{} 
+
+void SLOSBot::depth_img_cb(sensor_msgs::ImageConstPtr img) {}
+void SLOSBot::rgb_img_cb(sensor_msgs::ImageConstPtr img) {
+    std::cout << "recieved im" << std::endl;
+}
 
 void SLOSBot::search_for_object() {
     // get image
@@ -7,17 +21,26 @@ void SLOSBot::search_for_object() {
     // hsv segmentation
 
     // tell the mbot to rotate
+    lcm_to_ros::mbot_motor_command_t msg;
+    msg.angular_v = 0.3;
+    motor_command_pub.publish(msg);
 
 }
 
 void SLOSBot::execute_sm() {
-    switch(state) {
-        case State::SEARCH_FOR_OBJECT:
-            search_for_object();
-            break;
-        default:
-            std::cout << "invalid state" << std::endl;
-    };
+    ros::Rate loop_rate(10);
+    while(ros::ok()) {
+        switch(state) {
+            case State::SEARCH_FOR_OBJECT:
+                search_for_object();
+                break;
+            default:
+                std::cout << "invalid state" << std::endl;
+        };
+
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
 
 }
 
