5773b99ae02ac5f1ad2c4c85d0270c7474cdbb0c
Ashwin Gupta <ashwingupta2000@gmail.com>
2023-03-23 12:53:50 -0400

    pcl and stuff

--- slos_bot/include/slosbot.h
+++ slos_bot/include/slosbot.h
@@ -1,9 +1,15 @@
 #include "ros/node_handle.h"
 #include "ros/ros.h"
 #include "sensor_msgs/Image.h"
+#include "sensor_msgs/PointCloud2.h"
 #include <image_transport/image_transport.h>
 #include <opencv2/opencv.hpp>
 #include "opencv2/imgproc/imgproc.hpp"
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_types.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/conversions.h>
+#include <pcl_ros/transforms.h>
 
 class SLOSBot 
 {
@@ -23,13 +29,17 @@ class SLOSBot
 
         ros::Subscriber depth_img_sub;
         ros::Subscriber rgb_img_sub;
+        ros::Subscriber pointcloud_sub;
 
         cv::Mat cur_rgb;
         cv::Mat cur_depth;
+        //sensor_msgs::PointCloud2 cur_pc;
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cur_pc;
 
         void search_for_object();
 
 
         void depth_img_cb(sensor_msgs::ImageConstPtr img);
         void rgb_img_cb(sensor_msgs::ImageConstPtr img);
+        void pointcloud_cb(sensor_msgs::PointCloud2Ptr pc);
 };
--- slos_bot/src/main.cpp
+++ slos_bot/src/main.cpp
@@ -1,6 +1,9 @@
+#include <algorithm>
 #include <iostream>
 #include "ros/init.h"
 #include "ros/publisher.h"
+#include "sensor_msgs/PointCloud2.h"
+#include "sensor_msgs/image_encodings.h"
 #include "slosbot.h"
 #include "lcm_to_ros/mbot_motor_command_t.h"
 #include "cv_bridge/cv_bridge.h"
@@ -8,10 +11,29 @@
 SLOSBot::SLOSBot() :
  motor_command_pub( nh.advertise<lcm_to_ros::mbot_motor_command_t>("lcm_to_ros/MBOT_MOTOR_COMMAND", 1)),
  depth_img_sub( nh.subscribe("camera/depth_registered/image_raw", 1, &SLOSBot::depth_img_cb, this) ),
- rgb_img_sub( nh.subscribe("camera/rgb/image_rect_color", 1, &SLOSBot::rgb_img_cb, this) ) 
+ rgb_img_sub( nh.subscribe("camera/rgb/image_rect_color", 1, &SLOSBot::rgb_img_cb, this) ),
+ pointcloud_sub( nh.subscribe("camera/depth_registered/points", 1, &SLOSBot::pointcloud_cb, this) ) 
 {} 
 
-void SLOSBot::depth_img_cb(sensor_msgs::ImageConstPtr img) {}
+void SLOSBot::depth_img_cb(sensor_msgs::ImageConstPtr img) {
+    try {
+        cv_bridge::CvImagePtr im;
+        im = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::TYPE_32FC1);
+        cur_depth = im->image;
+    } catch(cv_bridge::Exception) {
+        std::cout << "Could not convert image" << std::endl;
+    }
+
+}
+
+void SLOSBot::pointcloud_cb(sensor_msgs::PointCloud2Ptr pc) {
+    pcl::PCLPointCloud2 pcl_pc2;
+    pcl_conversions::toPCL(*pc,pcl_pc2);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::fromPCLPointCloud2(pcl_pc2,*temp_cloud);
+    cur_pc = temp_cloud;
+}
+
 void SLOSBot::rgb_img_cb(sensor_msgs::ImageConstPtr img) {
     std::cout << "recieved im" << std::endl;
     try {
@@ -21,21 +43,17 @@ void SLOSBot::rgb_img_cb(sensor_msgs::ImageConstPtr img) {
     } catch(cv_bridge::Exception) {
         std::cout << "Could not convert image" << std::endl;
     }
-
-    
 }
 
 void SLOSBot::search_for_object() {
     using namespace cv;
     using namespace std;
 
-    // get image
+    // Make sure there is a current image
     if(cur_rgb.rows == 0 || cur_rgb.cols == 0) return;
-
     Mat debug_img = cur_rgb.clone();
 
-    // hsv segmentation //cv::imshow("img", cur_rgb);
-    //cv::waitKey(0);
+    // HSV segmentation
     Mat hsv_img;
     cvtColor(cur_rgb, hsv_img, COLOR_BGR2HSV);
 
@@ -47,20 +65,13 @@ void SLOSBot::search_for_object() {
     kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
     morphologyEx(bin_img, bin_img, MORPH_CLOSE, kernel);
 
-    // each object's contour is a list of points. We need a list of contours
+    // Contour detection
     vector<vector<Point> > contours; 
-    // The hiearchy contains informations about how objects are nested, we don't need it
     vector<Vec4i> hierarchy; 
-    // Find the contours and draw all of them (-1)
     findContours(bin_img, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_NONE, Point(0, 0));
-    //drawContours(debug_img, contours, -1, Scalar(0, 0, 255), 3);
+    auto hulls = contours;
 
-    vector<vector<Point> > hulls(contours.size());
-    for (int i = 0; i < contours.size(); i++) {
-        convexHull(contours[i], hulls[i]);
-    }
-
-    //Compute object angles using moments 
+    //Compute object sizes using moments and pick the largest object 
     vector<tuple<Moments, vector<Point>>> detections;
     for (auto& hull : hulls) {
         detections.push_back(std::make_tuple(moments(hull, true), std::move(hull)));
@@ -74,7 +85,14 @@ void SLOSBot::search_for_object() {
     );
     if(v != detections.end()) {
         auto& [m, h] = *v;
-        circle(debug_img, Point(m.m10/m.m00, m.m01/m.m00), 4, Scalar(255, 255, 0), -1);
+        circle(debug_img, Point(m.m10/m.m00, m.m01/m.m00), 4, Scalar(255, 255, 0), -1); 
+        std::vector<vector<Point>> temp = {h};
+        drawContours(debug_img, temp, 0, Scalar(0, 0, 255), 3);
+
+        // search for max min
+        auto min_pt_it = std::min_element(h.begin(), h.end(), [] (auto& p1, auto& p2) { return p1.x < p2.x; });
+        auto max_pt_it = std::max_element(h.begin(), h.end(), [] (auto& p1, auto& p2) { return p1.x < p2.x; });
+        line(debug_img, *min_pt_it, *max_pt_it, Scalar(255, 0, 0), 3);
 
         // tell the mbot to stop
         lcm_to_ros::mbot_motor_command_t msg;
@@ -83,14 +101,15 @@ void SLOSBot::search_for_object() {
     } else {
         // tell the mbot to rotate
         lcm_to_ros::mbot_motor_command_t msg;
-        msg.angular_v = 1.0;
+        msg.angular_v = 1.5;
         motor_command_pub.publish(msg);
     }
 
-    //imshow("post filter", bin_img);
-    //imshow("pre filter", debug_img);
+    imshow("post filter", bin_img);
+    imshow("pre filter", debug_img);
+    imshow("cur depth", cur_depth);
     //imwrite("/home/ashwin/Desktop/peepee.jpg", cur_rgb);
-    //waitKey(10);
+    waitKey(10);
 
 
 
