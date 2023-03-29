9b02ac5ba09555b8a3cdcdc452aa29df1c7de024
Ashwin Gupta <ashmg@umich.edu>
2023-03-29 15:05:15 -0400

    created claw controller and service message

---
+++ slos_bot/claw_controller.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+from gpiozero import Device, AngularServo
+from gpiozero.pins.pigpio import PiGPIOFactory
+import rospy
+from slosbot.srv import ControlClaw, ControlClawResponse
+
+OPEN_ANGLE = -60
+CLOSED_CAN_ANGLE = -10
+CLOSED_BOX_ANGLE = -20
+
+class ClawController:
+    def __init__(self):
+        Device.pin_factory = PiGPIOFactory()
+        servo = AngularServo(12)
+        rospy.init_node("claw_controller")
+        s = rospy.Service("control_claw", ControlClaw, set_claw_angle)
+        
+    def set_claw_angle(self, req: ControlClaw):
+        angle = None
+        if req.state == req.OPEN:
+            angle = OPEN_ANGLE
+        elif req.state == req.CLOSED_CAN:
+            angle = CLOSED_CAN_ANGLE
+        elif req.state == req.CLOSED_BOX:
+            angle = CLOSED_BOX_ANGLE
+        else:
+            return ControlClawResponse(False)
+        
+        self.servo.angle = angle
+        return ControlClawResponse(self.servo.angle == angle)
+
+if __name__ == "__main__":
+    ClawController()
+    rospy.spin()
